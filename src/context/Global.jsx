9da3b5746a4ad40c6f830ec3c02ad90f06ecfb44
Adam Wolf <38081038+wolf-adam@users.noreply.github.com>
2023-11-19 23:50:00 +0100

    Revert "Feat: Basic components creation"

--- src/context/Global.jsx
+++ src/context/Global.jsx
@@ -1,40 +1,16 @@
-import { createContext, useReducer, useState } from "react";
-import { Status } from "../constants/constant";
-import alphabet from '../data/alphabet.json'
+import { createContext, useState } from "react";
 
 export const GlobalContext = createContext();
 
-const reducer = (state, action) => {
-    if (action.type === 'clicked') {
-        const index = state.letters.findIndex(element => element.value === action.value)
-
-        // Modify state
-        state.letters[index].clicked = true;
-
-        return {
-            letters: [
-                ...state.letters,
-            ]
-        };
-    }
-    throw Error('Unknown action.');
-}
-
 const GlobalProvider = ({ children }) => {
-    const letters = alphabet;
-    const enhancedLetters = letters.map(letter => ({ value: letter, clicked: false }))
-    const [state, dispatch] = useReducer(reducer, {
-        letters: enhancedLetters
+    const [state, setState] = useState({
+        letters: [],
     })
-    // TODO: Get default status from localStorage
-    const [status, setStatus] = useState(Status.START)
 
     return (
         <GlobalContext.Provider value={{
             state,
-            dispatch,
-            status,
-            setStatus,
+            setState,
         }} >
             {children}
         </GlobalContext.Provider >
