8eb8945b0b97774bf7127edb5b2b94cc4dcf015f
AdamW <adam.wolf.personal@gmail.com>
2023-11-20 23:06:54 +0100

    remove useContext from App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-// import { useContext } from 'react';
-
-// import { GlobalContext } from './context/Global';
 import Content from './components/Content/Content';
 import FooterButtonGroup from './components/FooterButtonGroup/FooterButtonGroup';
 import './App.css';
 import NavBar from './components/Navbar/Navbar';
 
 const App = () => {
-  // const { state } = useContext(GlobalContext);
-
   return (
     <div className='App'>
       <NavBar />
