5751ba0182c5b5ad7204902269da252716e84948
AdamW <adam.wolf.personal@gmail.com>
2023-12-03 19:25:20 +0100

    introduce `LetterBox` component

--- src/components/WordGuesserContainer/WordGuesserContainer.jsx
+++ src/components/WordGuesserContainer/WordGuesserContainer.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 
 import { GlobalContext } from "../../context/Global";
 import ResultPanel from "../ResultPanel/ResultPanel";
+import LetterBox from "./LetterBox";
 import './WordGuesserContainer.css';
 
 const WordGuesserContainer = () => {
@@ -12,6 +13,11 @@ const WordGuesserContainer = () => {
             <ResultPanel />
             <div className='word-letter-button-group'>
                 {wordLetters.map((letter, index) =>
+                    <LetterBox
+                        key={`${letter}-${index}`}
+                        index={index}
+                        letter={letter}
+                    />
                 )}
             </div>
         </>
