09d6b5288a52788f7879e58d437b9e16c40f0ede
AdamW <adam.wolf.personal@gmail.com>
2023-12-05 21:26:04 +0100

    feat: introduce `Animation` component base

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Content from './components/Content/Content';
 import FooterButtonGroup from './components/FooterButtonGroup/FooterButtonGroup';
 import NavBar from './components/Navbar/Navbar';
+import Animation from './components/Animation/Animation';
 import './App.css';
 
 const App = () => {
@@ -8,7 +9,7 @@ const App = () => {
     <div className='App'>
       <NavBar />
       <div className='content'>
-        <div className='imagePlace'></div>
+        <Animation />
         <div className='input'>
           <h1>Hangman '23</h1>
           <Content />
