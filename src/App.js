97950bffb2d2cc8ecfbba17ee431bed160026012
AdamW <adam.wolf.personal@gmail.com>
2023-12-08 01:40:38 +0100

    fix #24

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import ResultPanel from './components/Content/ResultPanel/ResultPanel';
 import Animation from './components/Animation/Animation';
 import Content from './components/Content/Content';
 import FooterButtonGroup from './components/FooterButtonGroup/FooterButtonGroup';
@@ -5,13 +6,15 @@ import styles from './App.module.css';
 
 const App = () => {
   return (
-    <div className={styles.container}>
-      {/* <NavBar /> */}
-      <div className={styles.content}>
-        <Animation />
-        <div className={styles.input}>
-          <h1>Hangman '23</h1>
-          <Content />
+    <div className={styles.header}>
+      <div className={styles.container}>
+        <h1 className="mt-3">Hangman '23</h1>
+        <ResultPanel />
+        <div className={styles.content}>
+          <Animation />
+          <div className={styles.input}>
+            <Content />
+          </div>
         </div>
       </div>
       <FooterButtonGroup />
--- src/components/Content/WordChooser/WordChooser.jsx
+++ src/components/Content/WordChooser/WordChooser.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 
 import { GlobalContext } from "../../../context/Global";
-import { WORD_ITEM_NAME } from "../../../constants/constant";
+import { MAX_TRIES, WORD_ITEM_NAME } from "../../../constants/constant";
 import { makeUniqueArray } from "../../../utils";
 import wordsFromFile from '../../../data/hangman_words.json'
 import LetterButton from "../../common/LetterButton/LetterButton";
@@ -23,14 +23,24 @@ const WordChooser = () => {
 
     return (
         <div className={styles.container}>
-            {wordsLength.map(length => (
-                <LetterButton
-                    key={length}
-                    selected={word.length === length}
-                    text={length}
-                    onClick={() => setRandomWordByLength(length)}
-                />
-            ))}
+            <h2>Choose how long word you would like to guess</h2>
+            <div className={styles.letter_group}>
+                {wordsLength.map(length => (
+                    <LetterButton
+                        key={length}
+                        selected={word.length === length}
+                        text={length}
+                        onClick={() => setRandomWordByLength(length)}
+                    />
+                ))}
+            </div>
+            {word && (
+                <div className={styles.description}>
+                    <p>Random word was selected.</p>
+                    <p>{`You can have ${MAX_TRIES} wrong gueeses before the game ends.`}</p>
+                    <p> Good luck!</p>
+                </div>
+            )}
         </div>
     );
 };
